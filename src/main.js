2b4b56333b6e5cf3379af24a9f50c7eda7a44d7a
aatarasoff <aatarasoff@gmail.com>
2023-11-29 21:33:17 +0300

    first release

--- src/main.js
+++ src/main.js
@@ -1,28 +1,73 @@
 const core = require('@actions/core')
-const { wait } = require('./wait')
+const tc = require('@actions/tool-cache')
+const os = require('os')
+const io = require('@actions/io')
+const exec = require('@actions/exec')
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds', { required: true })
+  // Get inputs from workflow file
+  const version = core.getInput('version')
+  const pbuf_token = core.getInput('pbuf_token')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
-
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    // Fail the workflow run if an error occurs
-    core.setFailed(error.message)
+  if (version === '') {
+    core.setFailed('You must provide a version')
+    return
   }
+
+  core.info(`Installing pbuf-cli version ${version}`)
+
+  if (pbuf_token === '') {
+    core.info('No pbuf_token provided, skipping login')
+  }
+
+  // check that os is linux or darwin
+  if (os.platform() !== 'linux' && os.platform() !== 'darwin') {
+    core.setFailed(
+      'pbuf-cli can only be installed on Linux/Darwin at the moment'
+    )
+    return
+  }
+
+  // check that arch is amd64 or arm64
+  if (os.arch() !== 'x64' && os.arch() !== 'arm64') {
+    core.setFailed(
+      'pbuf-cli can only be installed on amd64 or arm64 at the moment'
+    )
+    return
+  }
+
+  // Construct the URL for the pbuf-cli binary
+  const arch = os.arch() === 'arm64' ? 'arm' : 'amd'
+  // add version without leading `v`
+  const releaserVersion = version.replace(/^v/, '')
+
+  const url = `https://github.com/pbufio/pbuf-cli/releases/download/${version}/pbuf-cli_${releaserVersion}_linux_${arch}64.tar.gz`
+
+  // Download the pbuf-cli binary
+  const downloadPath = await tc.downloadTool(url)
+
+  // Extract the downloaded file
+  const extractedPath = await tc.extractTar(downloadPath)
+
+  // Cache the extracted directory
+  const cachedPath = await tc.cacheDir(extractedPath, 'pbuf-cli', version)
+
+  // Add the cached directory to the PATH
+  core.addPath(cachedPath)
+
+  if ((await io.which('pbuf-cli', true)) === '') {
+    core.setFailed('pbuf-cli could not be found in the $PATH')
+    return
+  }
+
+  if (pbuf_token !== '') {
+    core.info(`Authenticate in to the pbuf registry`)
+
+    await exec.exec('pbuf-cli', ['init', 'pbufio/pbuf-setup-action'])
+    await exec.exec('pbuf-cli', ['auth', pbuf_token])
+  }
+
+  core.info(`Successfully setup pbuf-cli`)
 }
 
 module.exports = {
